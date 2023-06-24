a078e9a7188b49c5431b501880dd24e2c2326e62
Šimon Brandner <simon.bra.ag@gmail.com>
2023-06-24 17:54:29 +0200

    2023-06-24T17:54:29+02:00

--- scripts/zsh/plugins.zsh
+++ scripts/zsh/plugins.zsh
@@ -1,4 +1,3 @@
 source ~/Data1/GIT/Consuming/powerlevel10k/powerlevel10k.zsh-theme
-#source /usr/share/zsh-antigen/antigen.zsh
 #source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 #source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
