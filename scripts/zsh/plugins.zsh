f6a50ae8dd0513a6711cc7120a1c2222ca5723f7
Šimon Brandner <simon.bra.ag@gmail.com>
2023-07-14 09:23:24 +0200

    2023-07-14T09:23:24+02:00

--- scripts/zsh/plugins.zsh
+++ scripts/zsh/plugins.zsh
@@ -1,3 +1,7 @@
 source ~/Data1/GIT/Consuming/powerlevel10k/powerlevel10k.zsh-theme
-#source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-#source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+if [ ! -d "/etc/nixos" ] 
+then
+	source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+	source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+fi
