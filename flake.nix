4499245f2e017e073f342a93f5c760264cd5ea0d
Šimon Brandner <simon.bra.ag@gmail.com>
2024-04-07 10:05:10 +0200

    Flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-howdy.url = "github:fufexan/nixpkgs/6c4d8308df71d57f5a56f3037867355a3858be33";
+    hyprland.url = "github:hyprwm/Hyprland";
+    hypridle.url = "github:hyprwm/hypridle";
+    ags.url = "github:Aylur/ags";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+  };
+  outputs = { nixpkgs, hyprland, ... }@inputs: {
+    nixosConfigurations = {
+      "TuxedoInfinityBook14Gen8" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs.inputs = inputs;
+        modules = [
+          ./nixos/global.nix
+          ./nixos/machines/TuxedoInfinityBook14Gen8.nix
+        ];
+      };
+      "ThinkpadT430S" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs.inputs = inputs;
+        modules = [
+          ./nixos/global.nix
+          ./nixos/machines/ThinkpadT430S.nix
+        ];
+      };
+    };
+  };
+}
