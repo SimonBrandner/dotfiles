ea6b243bca69182c920268afd235150aed5e7fa8
Šimon Brandner <simon.bra.ag@gmail.com>
2024-04-08 09:28:40 +0200

    2024-04-08T09:28:40+02:00

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-howdy.url = "github:fufexan/nixpkgs/6c4d8308df71d57f5a56f3037867355a3858be33";
     hyprland.url = "github:hyprwm/Hyprland";
-    hypridle.url = "github:hyprwm/hypridle";
     ags.url = "github:Aylur/ags";
     home-manager.url = "github:nix-community/home-manager/release-23.11";
   };
