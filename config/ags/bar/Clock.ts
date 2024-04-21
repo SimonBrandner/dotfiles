bb0a4494af7c68952e86b619b930872c35c5541a
Šimon Brandner <simon.bra.ag@gmail.com>
2024-04-21 20:19:02 +0200

    2024-04-21T20:19:02+02:00

--- config/ags/bar/Clock.ts
+++ config/ags/bar/Clock.ts
@@ -10,17 +10,19 @@ const time = Utils.derive([clock], (c) => c.format(TIME_FORMAT) || "");
 const date = Utils.derive([clock], (c) => c.format(DATE_FORMAT) || "");
 
 export const Clock = () =>
-	Widget.Box({
+	Widget.Button({
 		class_name: "Clock",
-		vertical: true,
-		children: [
-			Widget.Label({ class_name: "Time" }).hook(
-				time,
-				(self) => (self.label = time.value),
-			),
-			Widget.Label({ class_name: "Date" }).hook(
-				date,
-				(self) => (self.label = date.value),
-			),
-		],
+		child: Widget.Box({
+			vertical: true,
+			children: [
+				Widget.Label({ class_name: "Time" }).hook(
+					time,
+					(self) => (self.label = time.value),
+				),
+				Widget.Label({ class_name: "Date" }).hook(
+					date,
+					(self) => (self.label = date.value),
+				),
+			],
+		}),
 	});
