f9893983a5a30861d8f3a558191310e809724117
Šimon Brandner <simon.bra.ag@gmail.com>
2024-04-24 18:15:48 +0200

    2024-04-24T18:15:48+02:00

--- scripts/chrome.zsh
+++ scripts/chrome.zsh
@@ -1,1 +1,1 @@
-google-chrome-stable --remote-debugging-port=9222 --enable-features=TouchpadOverscrollHistoryNavigation
+google-chrome-stable --remote-debugging-port=9222 --enable-features=TouchpadOverscrollHistoryNavigation --ozone-platform=wayland
