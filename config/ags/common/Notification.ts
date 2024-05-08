098409818a755221b30dfa662e62ff331264b2a2
Šimon Brandner <simon.bra.ag@gmail.com>
2024-05-08 09:41:32 +0200

    2024-05-08T09:41:32+02:00

--- config/ags/common/Notification.ts
+++ config/ags/common/Notification.ts
@@ -3,30 +3,24 @@ import { doesFileExist } from "utils";
 
 const FILE_PROTOCOL_PREFIX = "file://";
 
-const imageExists = (path?: string): boolean => {
-	if (!path) return false;
+const showImage = (notification: Notif): boolean => {
+	const { image, app_icon } = notification;
 
-	if (path.startsWith(FILE_PROTOCOL_PREFIX)) {
-		console.log("Starts with");
-		if (!doesFileExist(path.slice(FILE_PROTOCOL_PREFIX.length))) {
+	if (!image) return false;
+	if (app_icon === image) return false;
+
+	if (image.startsWith(FILE_PROTOCOL_PREFIX)) {
+		if (!doesFileExist(image.slice(FILE_PROTOCOL_PREFIX.length))) {
 			return false;
 		}
 	} else {
-		if (!doesFileExist(path)) return false;
+		if (!doesFileExist(image)) return false;
 	}
 
 	return true;
 };
 
-const NotificationIcon = ({ app_entry, app_icon, image }: Notif) => {
-	if (imageExists(image)) {
-		return Widget.Box({
-			vpack: "start",
-			class_name: "Image",
-			css: `background-image: url("${image}");`,
-		});
-	}
-
+const Icon = ({ app_entry, app_icon }: Notif) => {
 	let icon = "dialog-information-symbolic";
 	if (Utils.lookUpIcon(app_icon)) icon = app_icon;
 
@@ -39,71 +33,102 @@ const NotificationIcon = ({ app_entry, app_icon, image }: Notif) => {
 	});
 };
 
-export const Notification = (notification: Notif) => {
+const CloseButton = (notification: Notif) =>
+	Widget.Button({
+		class_name: "Close",
+		child: Widget.Icon({
+			icon: "window-close-symbolic",
+		}),
+		on_clicked: () => {
+			notification.close();
+		},
+	});
+
+const Title = (summary: string) =>
+	Widget.Label({
+		class_name: "Title",
+		xalign: 0,
+		justification: "left",
+		hexpand: true,
+		max_width_chars: 24,
+		truncate: "end",
+		wrap: true,
+		label: summary,
+		use_markup: true,
+	});
+
+const Actions = (notification: Notif) =>
+	Widget.Box({
+		class_name: "Actions",
+		children: notification.actions.map(({ id, label }) =>
+			Widget.Button({
+				class_name: "ActionButton",
+				hexpand: true,
+				child: Widget.Label(label),
+				on_clicked: () => {
+					notification.invoke(id);
+					notification.close();
+				},
+			}),
+		),
+	});
+
+const AppName = (name: string) =>
+	Widget.Label({
+		class_name: "AppName",
+		label: name,
+		hexpand: true,
+		xalign: 0,
+	});
+
+const Image = (notification: Notif) => {
+	if (!showImage(notification)) return;
+
 	return Widget.Box({
-		class_names: ["Nnixpkgs_5otification", notification.urgency],
+		vpack: "start",
+		class_name: "Image",
+		css: `background-image: url("${notification.image}");`,
+	});
+};
+
+const Body = (text: string) =>
+	Widget.Label({
+		class_name: "Body",
+		use_markup: true,
+		justification: "left",
+		label: text,
+		wrap: true,
+		xalign: 0,
+		truncate: "end",
+		lines: 2,
+	});
+
+export const Notification = (notification: Notif) => {
+	const image = Image(notification);
+	const actions = Actions(notification);
+
+	const titleBar = Widget.Box({
+		class_name: "TopBar",
+		children: [
+			Icon(notification),
+			AppName(notification.app_name),
+			CloseButton(notification),
+		],
+	});
+	const textContent = Widget.Box({
+		class_name: "TextContent",
+		vertical: true,
+		children: [Title(notification.summary), Body(notification.body)],
+	});
+	const content = Widget.Box({
+		class_name: "Content",
+		children: image ? [textContent, image] : [textContent],
+	});
+
+	return Widget.Box({
+		class_names: ["Notification", notification.urgency],
 		vertical: true,
 		attribute: { id: notification.id },
-		children: [
-			Widget.Box({
-				children: [
-					NotificationIcon(notification),
-					Widget.Box({
-						class_name: "Content",
-						vertical: true,
-						children: [
-							Widget.Box({
-								children: [
-									Widget.Label({
-										class_name: "Title",
-										xalign: 0,
-										justification: "left",
-										hexpand: true,
-										max_width_chars: 24,
-										truncate: "end",
-										wrap: true,
-										label: notification.summary,
-										use_markup: true,
-									}),
-									Widget.Button({
-										class_name: "Close",
-										child: Widget.Icon({
-											icon: "window-close-symbolic",
-										}),
-										on_clicked: () => {
-											notification.close();
-										},
-									}),
-								],
-							}),
-							Widget.Label({
-								class_name: "Body",
-								use_markup: true,
-								justification: "left",
-								label: notification.body,
-								wrap: true,
-								xalign: 0,
-								truncate: "end",
-								lines: 3,
-							}),
-						],
-					}),
-				],
-			}),
-			Widget.Box({
-				class_name: "Actions",
-				children: notification.actions.map(({ id, label }) =>
-					Widget.Button({
-						class_name: "ActionButton",
-						hexpand: true,
-						child: Widget.Label(label),
-						on_clicked: () => {
-							notification.invoke(id);
-							notification.close();
-						},
-					}),
-				),
-			}),
-		],
+		children: [titleBar, content, actions],
 	});
 };
