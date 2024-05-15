6491d0ed73fedb2498b03f8c0a5a5a6d7c6a8de8
Šimon Brandner <simon.bra.ag@gmail.com>
2024-05-15 21:02:29 +0200

    2024-05-15T21:02:29+02:00

---
+++ nixos/machines/Workstation/home.nix
@@ -0,0 +1,10 @@
+{ config, ... }: {
+  imports = [
+    ../../home.nix
+  ];
+  home.file = {
+    ".config/hypr/device.conf" = {
+      source = config.lib.file.mkOutOfStoreSymlink "/home/simon/dotfiles/config/hypr/Workstation.conf";
+    };
+  };
+}
