2cf292eec4fa12a96a35bafa9e35c5b471f751b7
Šimon Brandner <simon.bra.ag@gmail.com>
2024-06-13 14:31:05 +0200

    2024-06-13T14:31:05+02:00

--- config/nvim/lua/keymap.lua
+++ config/nvim/lua/keymap.lua
@@ -16,7 +16,6 @@ vim.keymap.set('n', '<leader>wh', ':wincmd h<CR>')
 vim.keymap.set('n', '<leader>wj', ':wincmd j<CR>')
 vim.keymap.set('n', '<leader>wk', ':wincmd k<CR>')
 vim.keymap.set('n', '<leader>wl', ':wincmd l<CR>')
-vim.keymap.set('n', '<leader>wrh', ':wincmd <<CR>')
 
 -- Terminal
 vim.cmd(":tnoremap <Esc> <C-\\><C-n>")
