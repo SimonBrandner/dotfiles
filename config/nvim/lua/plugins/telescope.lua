155b1e767f0683639691552cccf3ece3b8daa695
Šimon Brandner <simon.bra.ag@gmail.com>
2024-06-14 12:05:54 +0200

    2024-06-14T12:05:54+02:00

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -5,7 +5,7 @@ return {
 		dependencies = { 'nvim-lua/plenary.nvim' },
 		config = function()
 			local builtin = require("telescope.builtin")
-			vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+			vim.keymap.set('n', '<C-k>', builtin.find_files, {})
 			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 			vim.keymap.set('n', '<leader>lu', builtin.lsp_references, {})
 		end
