30c921c4f33413c8482f56ea4938c14777b7e17b
Šimon Brandner <simon.bra.ag@gmail.com>
2024-06-20 20:45:34 +0200

    2024-06-20T20:45:34+02:00

--- config/nvim/lua/plugins/spelunker.lua
+++ config/nvim/lua/plugins/spelunker.lua
@@ -1,8 +1,10 @@
 return {
 	"kamykn/spelunker.vim",
-	dependencies = {"kamykn/popup-menu.nvim"},
-	config = function ()
-		vim.cmd("highlight SpelunkerSpellBad cterm=undercurl ctermfg=247 gui=undercurl guifg=NONE guisp=red")
-		vim.cmd("highlight SpelunkerComplexOrCompoundWord cterm=undercurl ctermfg=NONE gui=undercurl guifg=NONE guisp=gray")
-	end
+	dependencies = { "kamykn/popup-menu.nvim" },
+	config = function()
+		vim.cmd("highlight SpelunkerSpellBad cterm=undercurl ctermfg=247 gui=undercurl guifg=NONE guisp=gray")
+		vim.cmd(
+			"highlight SpelunkerComplexOrCompoundWord cterm=undercurl ctermfg=NONE gui=undercurl guifg=NONE guisp=gray"
+		)
+	end,
 }
