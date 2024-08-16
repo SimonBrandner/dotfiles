bb6f2ea26faf8e6eeac800175c8cc30424e3180e
Šimon Brandner <simon.bra.ag@gmail.com>
2024-08-16 10:27:44 +0200

    Use ppm
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- scripts/screenshot.zsh
+++ scripts/screenshot.zsh
@@ -2,5 +2,5 @@
 output="$(hyprctl monitors | grep "ID $(hyprctl activeworkspace | grep monitorID | awk '{print $2}')" | awk '{print $2}')"
 file="$XDG_PICTURES_DIR/Screenshots/Screenshot_%Y-%m-%d_%H:%M:%S.png"
 
-grim -o $output - | satty -f - --output-filename $file
+grim -t ppm -o $output - | satty -f - --output-filename $file
 
