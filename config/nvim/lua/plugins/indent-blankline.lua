3b69d90b421c88561c4fd9f28f05b4cfd5ae0871
Šimon Brandner <simon.bra.ag@gmail.com>
2024-08-18 10:47:02 +0200

    Add `indent-blankline`
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

---
+++ config/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,12 @@
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	config = function()
+		require("ibl").setup({
+			scope = {
+				enabled = true,
+				highlight = { "Function", "Label" },
+			},
+		})
+	end,
+}
