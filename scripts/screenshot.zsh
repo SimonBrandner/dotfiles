6bb04448e582018be90c8868142924bbba75cf52
Šimon Brandner <simon.bra.ag@gmail.com>
2024-12-21 15:34:50 +0100

    Make screenshoting work on Sway
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- scripts/screenshot.zsh
+++ scripts/screenshot.zsh
@@ -1,5 +1,11 @@
 #!/usr/bin/env zsh
-output="$(hyprctl monitors | grep "ID $(hyprctl activeworkspace | grep monitorID | awk '{print $2}')" | awk '{print $2}')"
+
+if [ -z "$HYPRLAND_INSTANCE_SIGNATURE" ]; then
+	output="$(swaymsg -r -t get_outputs | jq -r '.[] | select(.focused == true) | .name')"
+else
+	output="$(hyprctl monitors | grep "ID $(hyprctl activeworkspace | grep monitorID | awk '{print $2}')" | awk '{print $2}')"
+fi
+
 file="$XDG_PICTURES_DIR/Screenshots/Screenshot_%Y-%m-%d_%H:%M:%S.png"
 
 grim -t ppm -o $output - | satty -f - --output-filename $file
