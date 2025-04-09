d9e396c2c354a26b2e6ec5d6de0fc5c20e423d43
Šimon Brandner <simon.bra.ag@gmail.com>
2025-04-09 08:50:30 +0200

    Goodbye Hyprland
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- config/ags/bar/Workspaces.ts
+++ config/ags/bar/Workspaces.ts
@@ -1,15 +1,7 @@
 import { Gtk, Widget } from "astal/gtk3";
 import { bind, exec } from "astal";
-import Hyprland from "gi://AstalHyprland";
 
 import Sway, { Workspace } from "../services/Sway.ts";
-
-let hyprland = null;
-try {
-	hyprland = Hyprland.get_default();
-} catch {
-	hyprland = null;
-}
 
 let sway = null;
 try {
@@ -19,9 +11,7 @@ try {
 }
 
 export const Workspaces = () => {
-	if (hyprland) {
-		return HyprlandWorkspaces();
-	} else if (sway) {
+	if (sway) {
 		return SwayWorkspaces();
 	} else {
 		throw "No known window manager running";
@@ -46,43 +36,5 @@ export const SwayWorkspaces = () => {
 					),
 				})
 		),
-	});
-};
-
-export const HyprlandWorkspaces = () => {
-	const dispatch = (workspace_index: number) =>
-		hyprland.messageAsync(`dispatch workspace ${workspace_index}`);
-
-	return new Widget.Box({
-		child: new Widget.Box({
-			class_name: "Workspaces",
-			children: Array.from({ length: 11 }, (_, i) => i + 1).map(
-				(i) =>
-					new Widget.Button({
-						className: "Workspace",
-						attribute: i,
-						label: `${i - 1}`, // I prefer to have a 0th workspace and this is
-						// a hack to achieve it
-						valign: Gtk.Align.CENTER,
-						onClicked: () => dispatch(i),
-						setup: (self) =>
-							self.hook(hyprland, () => {
-								if (hyprland.active.workspace.id == self.attribute) {
-									self.toggleClassName("Active", true);
-								} else {
-									self.toggleClassName("Active", false);
-								}
-							}),
-					})
-			),
-			setup: (self) =>
-				self.hook(hyprland, () =>
-					self.children.forEach((workspace_button) => {
-						workspace_button.visible = hyprland.workspaces.some(
-							(ws) => ws.id === workspace_button.attribute
-						);
-					})
-				),
-		}),
 	});
 };
