f5e876fa5eabbc5f1694ce538d157ef8aa3689f3
Šimon Brandner <simon.bra.ag@gmail.com>
2026-02-26 18:47:31 +0100

    Handle text file spell check
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- config/nvim/lsp/harper-ls.lua
+++ config/nvim/lsp/harper-ls.lua
@@ -1,6 +1,6 @@
 return {
 	cmd = { "harper-ls", "--stdio" },
-	filetypes = { "typst", "markdown", "tex" },
+	filetypes = { "typst", "markdown", "tex", "text" },
 	settings = {
 		["harper-ls"] = {
 			userDictPath = "",
