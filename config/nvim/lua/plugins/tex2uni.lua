43268932b8c3d1365a7abe04cc4060562d3fef3b
Šimon Brandner <simon.bra.ag@gmail.com>
2026-02-26 18:47:47 +0100

    Enable tex2uni for Rocq
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- config/nvim/lua/plugins/tex2uni.lua
+++ config/nvim/lua/plugins/tex2uni.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"Zeta611/tex2uni.nvim",
 		opts = {
-			ft = { "*.{agda}", "*.{lagda.md}", "*.{lagda.tex}", "*.{lagda.typ}" }, -- enable for specific filetypes
+			ft = { "*.{agda}", "*.{lagda.md}", "*.{lagda.tex}", "*.{lagda.typ}", "*.{v}" }, -- enable for specific filetypes
 			abbreviations = {
 				extra = {
 					["pure"] = "⌜$CURSOR⌝", -- add custom abbreviations
