42223d793c73da4cb96d8af1e18f1fe57f811ffd
Šimon Brandner <simon.bra.ag@gmail.com>
2026-06-01 11:59:23 +0200

    Fix screenshots in Niri
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- scripts/screenshot.zsh
+++ scripts/screenshot.zsh
@@ -1,6 +1,8 @@
 #!/usr/bin/env zsh
 
-if [ -z "$HYPRLAND_INSTANCE_SIGNATURE" ]; then
+if [ -n "$NIRI_SOCKET" ]; then
+	output="$(niri msg -j focused-output | jq -r '.name')"
+elif [ -z "$HYPRLAND_INSTANCE_SIGNATURE" ]; then
 	output="$(swaymsg -r -t get_outputs | jq -r '.[] | select(.focused == true) | .name')"
 else
 	output="$(hyprctl monitors | grep "ID $(hyprctl activeworkspace | grep monitorID | awk '{print $2}')" | awk '{print $2}')"
@@ -9,4 +11,3 @@ fi
 file="$XDG_PICTURES_DIR/Screenshots/Screenshot_%Y-%m-%d_%H:%M:%S.png"
 
 grim -t ppm -o $output - | satty -f - --output-filename $file
-
