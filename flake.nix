d612c6c32e8f129c89f1a74930d7b88f666ad28a
Šimon Brandner <simon.bra.ag@gmail.com>
2026-08-07 10:15:11 +0200

    Upgrade Astal and point everything to a single nixpkgs
    Fixes https://github.com/SimonBrandner/dotfiles/issues/52

--- flake.nix
+++ flake.nix
@@ -1,14 +1,22 @@
 {
   inputs = {
-    # NixOS
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-discord.url = "github:NixOS/nixpkgs/03b3121df26689191c0cb63ea419ff50741c0bd7";
-    home-manager.url = "github:nix-community/home-manager/release-26.05";
-
-    # AGS/Astal (shell)
-    astal.url = "github:aylur/astal";
-    ags.url = "github:Aylur/ags/v3.1.2";
-
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    astal = {
+      url = "github:aylur/astal";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    ags = {
+      url = "github:Aylur/ags/v3.1.2";
+      inputs = {
+        astal.follows = "astal";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
     # Oblichey first needs to be fixed
     # oblichey.url = "path:/home/simon/GIT/Consuming/oblichey";
   };
